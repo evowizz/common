35f5e9c3a3e5574db219b2ac5acc1633dff39a52
Dylan Roussel <mail@evowizz.dev>
2022-09-07 09:44:00 +0200

    Add missing ui-tooling dependency

--- app-compose/build.gradle.kts
+++ app-compose/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.material.iconsExtended)
     api(libs.androidx.compose.material3)
+    debugApi(libs.androidx.compose.ui.tooling)
     api(libs.androidx.compose.ui.tooling.preview)
     api(libs.androidx.compose.ui.util)
     api(libs.androidx.compose.runtime)
